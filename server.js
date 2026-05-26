768225d8c63bb13c922d7c6bdd05b3b8abf83b03
Montyyy <danyramadhan558@gmail.com>
2026-05-26 21:25:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
-
 app.use(express.json()); 
 
 mongoose.connect('mongodb://127.0.0.1:27017/bookmark_db')
-    .then(() => console.log('Terhubung ke database Bookmark!'))
+    .then(() => console.log('Terhubung ke database'))
     .catch((err) => console.log('Gagal terhubung:', err));
 
 const bookmarkSchema = new mongoose.Schema({
@@ -29,7 +28,6 @@ const bookmarkSchema = new mongoose.Schema({
         default: Date.now 
     }
 });
-
 
 const Bookmark = mongoose.model('Bookmark', bookmarkSchema);
 app.post('/bookmarks', async (req, res) => {
